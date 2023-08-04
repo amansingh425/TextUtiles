5974ad4b91b9637d520055b7ebc4f826dba232f4
Aman <amansingh28112002@gmail.com>
2023-08-04 23:50:45 +0530

    Updates

---
+++ precache-manifest.3e780ddf399cc3138a0b698d874ae238.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e951afee9f2bfe67838c",
+    "url": "/TextUtiles/static/css/main.9f1cd8a0.chunk.css"
+  },
+  {
+    "revision": "e951afee9f2bfe67838c",
+    "url": "/TextUtiles/static/js/main.e951afee.chunk.js"
+  },
+  {
+    "revision": "3bc8ac50b02e373cc067",
+    "url": "/TextUtiles/static/js/1.3bc8ac50.chunk.js"
+  },
+  {
+    "revision": "d3c95b11633751fc3daa",
+    "url": "/TextUtiles/static/js/2.d3c95b11.chunk.js"
+  },
+  {
+    "revision": "39ce7c72bb2062d05f44",
+    "url": "/TextUtiles/static/js/runtime~main.39ce7c72.js"
+  },
+  {
+    "revision": "51ded4a093576497d013c94fbd0d9730",
+    "url": "/TextUtiles/index.html"
+  }
+];
